5760317dd74b6ab9cce0556337d62c0c5d13992d
Lucas Delobelle <lucas@ld-web.net>
2020-11-13 11:29:18 +0100

    Removed unnecessary use

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
-use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
