a4f98530e827b969fae519db25d594654cf4244c
Lucas Delobelle <lucas@ld-web.net>
2020-11-24 10:43:21 +0100

    Newsletter formulaire v1

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Newsletter;
+use App\Form\NewsletterType;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,14 +17,25 @@ class IndexController extends AbstractController
   /**
    * @Route("/", name="index")
    */
-  public function index(ArticleRepository $articleRepository): Response
+  public function index(Request $request, ArticleRepository $articleRepository, EntityManagerInterface $em): Response
   {
+    $newsletter = new Newsletter();
+    $form = $this->createForm(NewsletterType::class, $newsletter);
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $em->persist($newsletter);
+      $em->flush();
+    }
+    
     // 1 - Je récupère les articles en discutant avec ma couche de service
     $articles = $articleRepository->findTop(Article::NB_HOME);
     
     // 2 - Je transmets les articles à la vue que je souhaite afficher
     return $this->render('index/index.html.twig', [
       'articles' => $articles,
+      'newsletterForm' => $form->createView()
     ]);
   }
 
