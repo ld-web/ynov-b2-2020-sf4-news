2665ea36cc82312c19fc9b7ed726484e0ab48fba
Lucas Delobelle <lucas@ld-web.net>
2020-11-26 11:04:03 +0100

    Added User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,12 +3,22 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\User;
+use App\Entity\UserRole;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+  private $encoder;
+
+  public function __construct(UserPasswordEncoderInterface $encoder)
+  {
+    $this->encoder = $encoder;
+  }
+
   public function load(ObjectManager $manager)
   {
     $faker = Faker\Factory::create();
@@ -22,6 +32,18 @@ class AppFixtures extends Fixture
         ->setContent($faker->paragraph(24));
       $manager->persist($article);
     }
+
+    $user = new User();
+
+    $user->setEmail('lucas@ld-web.net')
+      ->setRoles([UserRole::ADMIN]);
+
+    $user->setPassword($this->encoder->encodePassword(
+      $user,
+      'blablabla'
+    ));
+
+    $manager->persist($user);
 
     $manager->flush();
   }
